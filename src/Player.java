4c8dea3b91569eda307515deaa0f32cf402bd233
JoJoPnz <act21078@gmail.com>
2021-04-09 22:43:35 +0700

    create Player class

---
+++ src/Player.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public abstract class Player {
+    private int money;
+    private String characterPictureLink;
+    private boolean turn;
+    private ArrayList<Property> propertyHave;
+    private String skill;
+
+    private boolean isBankrupt;
+
+
+    public Player() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+    public String getCharacterPictureLink() {
+        return characterPictureLink;
+    }
+
+    public ArrayList<Property> getCityHave() {
+        return propertyHave;
+    }
+
+    public void setCharacterPictureLink(String characterPictureLink) {
+        this.characterPictureLink = characterPictureLink;
+    }
+
+    public void setCityHave(ArrayList<Property> cityHave) {
+        this.propertyHave = cityHave;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public String getCharacterPicture() {
+        return characterPictureLink;
+    }
+
+    public boolean isTurn() {
+        return turn;
+    }
+
+    public void setMoney(int money) {
+        if (money < 0) {
+            this.money = 0;
+        }
+        else {
+            this.money = money;
+        }
+    }
+
+    public void setCharacterPicture(String characterPicture) {
+        this.characterPictureLink = characterPicture;
+    }
+
+    public void setTurn(boolean turn) {
+        this.turn = turn;
+    }
+
+}
