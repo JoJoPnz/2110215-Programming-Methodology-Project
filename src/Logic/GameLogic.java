0b8ac1ecea34071a7ce66612a428b5c24c566ae3
Polapob <71330294+Polapob@users.noreply.github.com>
2021-04-13 15:10:46 +0700

    Gamelogic

--- src/Logic/GameLogic.java
+++ src/Logic/GameLogic.java
@@ -7,6 +7,10 @@ public class GameLogic {
 	public GameLogic() {
 		// TODO Auto-generated constructor stub
 		Player player1 = new Player("Abab");
+		Player player2 = new Player("Abac");
+		// initialize Starting turn
+		player1.setTurn(true);
+		
 	}
 
 }
