8dfa694e847b627b58ad53460933be91b3fa3f26
Polapob <71330294+Polapob@users.noreply.github.com>
2021-04-13 15:47:25 +0700

    Player with game Logic

--- src/Logic/GameLogic.java
+++ src/Logic/GameLogic.java
@@ -3,15 +3,13 @@ package Logic;
 import player.Player;
 
 public class GameLogic {
-	private static Player player1;
-	private static Player player2;
+	public static Player player1 = new Player("Abab");;
+	public static Player player2 = new Player("Abac");;
+	
 	public GameLogic() {
 		// TODO Auto-generated constructor stub
-		Player player1 = new Player("Abab");
-		Player player2 = new Player("Abac");
 		// initialize Starting turn
 		player1.setTurn(true);
-		
 	}
 	public static Player getPlayer1() {
 		return player1;
