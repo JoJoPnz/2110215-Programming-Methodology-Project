5d69e837ad527765cb1bb125244a30a38fd01b5e
JoJoPnz <act21078@gmail.com>
2021-05-14 16:38:48 +0700

    mainmenu

--- src/board/Board.java
+++ src/board/Board.java
@@ -5,12 +5,16 @@ package board;
 import Logic.GameLogic;
 import dice.DicePane;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -29,22 +33,13 @@ import javafx.stage.Stage;
 import property.Area;
 import property.Property;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
 
 public class Board extends Application {
 	@Override
 	public void start(Stage stage) {
-		
-		//====================Main menu scene=========================
-		VBox root = new VBox();
-		
-		root.setBackground(new Background(new BackgroundImage(new Image("menubg.jpg"), BackgroundRepeat.NO_REPEAT, 
-				BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(900,600,true,true,true,true))));
-		
-		Scene menuScene = new Scene(root, 900, 600);
-		
-		//================================================================
 		
 		HBox root2 = new HBox();
 		root2.setSpacing(10);
@@ -62,7 +57,16 @@ public class Board extends Application {
 		
 		// Creating a scene object
 		Scene gameScene = new Scene(root2, 1200, 950);
+		gameScene.getStylesheets().add("stylesheet2.css");
+		// Current Scene
+		Scene currentScene;
+		
+		//====================Main menu scene=========================
+		Scene menuScene = createmenuScene();
+		//================================================================
+		
 
+		
 		// Setting title to the Stage
 		stage.setTitle("Monopoly Game");
 
@@ -85,5 +89,43 @@ public class Board extends Application {
 
 	public static void main(String args[]) {
 		launch(args);
+	}
+	
+	public Scene createmenuScene() {
+		VBox root = new VBox();
+
+		root.setBackground(new Background(new BackgroundImage(new Image("menubg.jpg"), BackgroundRepeat.NO_REPEAT, 
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(900,550,true,true,true,true))));
+		ImageView logo = new ImageView(new Image("logo.png"));
+		logo.setFitHeight(300);
+		logo.setFitWidth(700);
+		root.setAlignment(Pos.CENTER);
+		
+		HBox buttonPane = new HBox();
+		Button startGame = new Button("Play");
+		Button exitGame = new Button("Exit");
+		
+		startGame.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				
+			}
+		});
+		
+		startGame.setFont(new Font("Ariel",50));
+		exitGame.setFont(new Font("Ariel",50));
+		startGame.setPrefWidth(300);
+		startGame.setPrefHeight(80);
+		exitGame.setPrefWidth(300);
+		exitGame.setPrefHeight(80);
+		buttonPane.setAlignment(Pos.CENTER);
+		buttonPane.setSpacing(50);
+		buttonPane.getChildren().addAll(startGame, exitGame);
+		
+		root.getChildren().addAll(logo,buttonPane);
+		Scene menuScene = new Scene(root, 900, 550);
+		menuScene.getStylesheets().add("stylesheet.css");
+		
+		return menuScene;
 	}
 }
