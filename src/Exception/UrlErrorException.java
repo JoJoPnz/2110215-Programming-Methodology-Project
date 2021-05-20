9716b49393fc16bf0e8c1151cf61450db6c489f7
JoJoPnz <act21078@gmail.com>
2021-05-20 10:57:23 +0700

    modify code in dicePane

--- src/Exception/UrlErrorException.java
+++ src/Exception/UrlErrorException.java
@@ -1,9 +1,8 @@
 package Exception;
 
 public class UrlErrorException extends Exception{
-
+	
 	public UrlErrorException() {
-		// TODO Auto-generated constructor stub
 		super("URL ERROR PLEASE TRY AGAIN.");
 	}
 
--- src/dice/DicePane.java
+++ src/dice/DicePane.java
@@ -21,6 +21,7 @@ import javafx.scene.text.Font;
 import player.Player;
 import square.PropertySquare;
 
+import java.util.ArrayList;
 
 import Logic.GameLogic;
 
@@ -32,10 +33,9 @@ public class DicePane extends VBox {
 	public static Button buyButton = new Button("Buy Area");
 	public static TextArea statusText = new TextArea();
 	public static Button exitButton = new Button("Exit");
-	
 	private Label diceLabel;
 	private static ImageView diceImage = new ImageView();
-	private static int faceValue;
+	private static int faceValue = 1;
 
 	public DicePane() {
 		this.setAlignment(Pos.CENTER);
@@ -44,108 +44,33 @@ public class DicePane extends VBox {
 		this.setBorder(new Border(
 				new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		
-		statusText.setText("==== Player 1 Turn ====\n\n"); // start text
-		statusText.setFont(new Font("Arial", 15));
-		statusText.setWrapText(true);
-		
-		diceLabel = new Label();
-		diceLabel.setFont(new Font("Arial", 20));
+		setStatusText();
 		setDiceLabelText();
-		diceLabel.setPadding(new Insets(60, 0, 0, 0));
 		
-		this.faceValue = 1;
+		ArrayList<Button> allButton = new ArrayList<>();
+		allButton.add(buyButton);
+		allButton.add(upgradeButton);
+		allButton.add(rollButton);
+		allButton.add(endTurnButton);
+		allButton.add(exitButton);
+		setButtonSize(allButton);
+		setStartButtonDisable();
+		setButtonAction();
+		
 		setDiceImage(faceValue); // default dice picture
 
-		buyButton.setDisable(true);
-		buyButton.setPrefWidth(150);
-		buyButton.setPrefHeight(50);
-		endTurnButton.setDisable(true);
-
-		rollButton.setPrefWidth(150);
-		rollButton.setPrefHeight(50);
-		rollButton.setDisable(false);
-		rollButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				GameLogic.rollDice(); // rollDice then change dicePicture and faceValue
-				GameLogic.move(); // move a player
-
-				// set button behavior
-				endTurnButton.setDisable(false);
-				rollButton.setDisable(true);
-				
-				// play sound
-				playSoundEffect("/rollDice.mp3");
-			}
-		});
-		// buyButton.setOnAction();
-		buyButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				GameLogic.buyArea();
-				buyButton.setDisable(true);
-				
-				// play sound
-				playSoundEffect("/buy.mp3");
-			}
-		});
-
-		
-		upgradeButton.setPrefWidth(150);
-		upgradeButton.setPrefHeight(50);
-		upgradeButton.setDisable(true);
-		upgradeButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				GameLogic.upgradeProperty();
-				upgradeButton.setDisable(true);
-				
-				// play sound
-				playSoundEffect("/upgrade.mp3");
-			}
-		});
-		
-
-
-		endTurnButton.setPrefWidth(150);
-		endTurnButton.setPrefHeight(50);
-		endTurnButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				GameLogic.endTurn(); // change turn to other player
-				clearStatusText(); // clear text
-				
-				// set button behavior
-				endTurnButton.setDisable(true);
-				rollButton.setDisable(false);
-				buyButton.setDisable(true);
-				upgradeButton.setDisable(true);
-			}
-		});
-		
-		exitButton.setPrefWidth(150);
-		exitButton.setPrefHeight(50);
-		exitButton.setPadding(new Insets(70, 0, 0, 0));
-		exitButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				System.exit(0);
-			}
-		});
-		
-		
 		VBox buttonPane = new VBox();
 		buttonPane.setSpacing(15);
 		buttonPane.setAlignment(Pos.CENTER);
 		buttonPane.getChildren().addAll(rollButton, buyButton, upgradeButton, endTurnButton);
 		this.getChildren().addAll(statusText, diceLabel, this.getDiceImage(), buttonPane, exitButton);
-
-
-
 	}
 
 	public void setDiceLabelText() {
+		diceLabel = new Label();
+		diceLabel.setFont(new Font("Arial", 20));
 		diceLabel.textProperty().setValue("DICE (1-6)");
+		diceLabel.setPadding(new Insets(60, 0, 0, 0));
 	}
 
 	public static void setDiceImage(int faceValue) {
@@ -155,7 +80,106 @@ public class DicePane extends VBox {
 		diceImage.setFitWidth(100);
 		diceImage.setFitHeight(100);
 	}
+	
+	private void setStatusText() {
+		statusText.setText("==== Player 1 Turn ====\n\n"); // start text
+		statusText.setFont(new Font("Arial", 15));
+		statusText.setWrapText(true);
+	}
+	
+	private void clearStatusText() {
+		// Set Header text
+		if (GameLogic.player1.isTurn()) {
+			statusText.setText("==== Player 1 Turn ====\n\n");
+		}
+		else {
+			statusText.setText("==== Player 2 Turn ====\n\n");
+		}
+		
+	}
+	
+	private void setButtonSize(ArrayList<Button> allButton) {
+		for (Button button : allButton) {
+			button.setPrefWidth(150);
+			button.setPrefHeight(50);
+		}
+		exitButton.setPadding(new Insets(70, 0, 0, 0));
+	}
+	
+	private void setStartButtonDisable() {
+		buyButton.setDisable(true);
+		endTurnButton.setDisable(true);
+		rollButton.setDisable(false);
+		upgradeButton.setDisable(true);
+	}
+	
+	private void setButtonAction() {
+		rollButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				GameLogic.rollDice(); // rollDice then change dicePicture and faceValue
+				GameLogic.move(); // move a player
+				// set button behavior
+				endTurnButton.setDisable(false);
+				rollButton.setDisable(true);
+				// play sound
+				playSoundEffect("/rollDice.mp3");
+			}
+		});
+		
+		buyButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				GameLogic.buyArea();
+				// set button behavior
+				buyButton.setDisable(true);
+				// play sound
+				playSoundEffect("/buy.mp3");
+			}
+		});	
+		
+		upgradeButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				GameLogic.upgradeProperty();
+				// set button behavior
+				upgradeButton.setDisable(true);
+				// play sound
+				playSoundEffect("/upgrade.mp3");
+			}
+		});
 
+		endTurnButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				GameLogic.endTurn(); // change turn to other player
+				clearStatusText(); // clear text
+				// set button behavior
+				endTurnButton.setDisable(true);
+				rollButton.setDisable(false);
+				buyButton.setDisable(true);
+				upgradeButton.setDisable(true);
+			}
+		});
+		
+		exitButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.exit(0); // close program
+			}
+		});	
+	}
+	
+	private void playSoundEffect(String url) {
+		try {
+			AudioClip buzzer = new AudioClip(getClass().getResource(url).toExternalForm());
+			buzzer.play();
+		}
+		catch (Exception e){
+			System.out.println("Soundtrack url doesn't exist.");
+		}
+	}
+	
 	public ImageView getDiceImage() {
 		return diceImage;
 	}
@@ -171,29 +195,6 @@ public class DicePane extends VBox {
 	public static Button getBuyButton() {
 		return buyButton;
 	}
-	
-	private void playSoundEffect(String url) {
-		try {
-			AudioClip buzzer = new AudioClip(getClass().getResource(url).toExternalForm());
-			buzzer.play();
-		}
-		catch (Exception e){
-			System.out.println("Soundtrack url doesn't exist.");
-		}
-	}
-	
-	private void clearStatusText() {
-		// Set Header text
-		if (GameLogic.player1.isTurn()) {
-			statusText.setText("==== Player 1 Turn ====\n\n");
-		}
-		else {
-			statusText.setText("==== Player 2 Turn ====\n\n");
-		}
-		
-	}
-	
-	
 	
 	
 }
